2ea69479c05dcec3ff25000902affc6f242452fd
Hassan Yousaf <hassan.yousaf@talkshot.com>
2020-09-02 11:21:13 +0500

    initial commit

---
+++ gin_firebase_auth.go
@@ -0,0 +1,76 @@
+package gin_firebase_auth
+
+import (
+	"context"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var (
+	tokenKey = "FIREBASE_ID_TOKEN"
+)
+
+type Config struct {
+	AuthOverride     *map[string]interface{}
+	DatabaseURL      string
+	ProjectID        string
+	ServiceAccountID string
+	StorageBucket    string
+}
+
+type FirebaseAuth struct {
+	auth *auth.Client
+}
+
+func New(cfg *Config) *FirebaseAuth {
+	var app *firebase.App
+	var err error
+	client := &FirebaseAuth{}
+	if cfg != nil {
+		app, err = firebase.NewApp(context.Background(), &firebase.Config{
+			AuthOverride:     cfg.AuthOverride,
+			DatabaseURL:      cfg.DatabaseURL,
+			ProjectID:        cfg.ProjectID,
+			ServiceAccountID: cfg.ServiceAccountID,
+			StorageBucket:    cfg.StorageBucket,
+		})
+	} else {
+		app, err = firebase.NewApp(context.Background(), nil)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.auth, err = app.Auth(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func (f *FirebaseAuth) Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		token := strings.Replace(authHeader, "Bearer ", "", 1)
+		idToken, err := f.auth.VerifyIDToken(context.Background(), token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"status":  http.StatusUnauthorized,
+				"message": err,
+			})
+		}
+		c.Set(tokenKey, idToken)
+		c.Next()
+	}
+}
+
+func ExtractClaims(c *gin.Context) *auth.Token {
+	token, ok := c.Get(tokenKey)
+	if !ok {
+		return new(auth.Token)
+	}
+	return token.(*auth.Token)
+}
